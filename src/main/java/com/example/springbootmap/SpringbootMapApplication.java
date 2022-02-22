2a7920d0fad11a0a2c3a544725b0d4b6a1ca313d
domin <dominikpatrzek21@gmail.com>
2022-02-22 14:41:16 +0100

    Initial commit - map added

---
+++ src/main/java/com/example/springbootmap/SpringbootMapApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootmap/SpringbootMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
